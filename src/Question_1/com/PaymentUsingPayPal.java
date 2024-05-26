420eedafd53100639aa07ad37dbd1f96044cb56a
shaila001 <rahamanshaila2001@gmail.com>
2024-05-26 10:17:30 +0600

    Paypal Class created succesfully

--- src/Question_1/com/PaymentUsingPayPal.java
+++ src/Question_1/com/PaymentUsingPayPal.java
@@ -1,10 +1,10 @@
 package Question_1.com;
-public class PaymentUsingPayPal {
+
     class PaymentUsingPayPal implements paymentType{
         @Override
         public boolean pay(double amount) {
-            System.out.println("Payment Through credit card");
+            System.out.println("Payment Through pay pal card");
             return true;
         }
     }
-}
+
