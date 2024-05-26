04ad4da753e86b8dc6fc57a3ed430b7122e6c326
shaila001 <rahamanshaila2001@gmail.com>
2024-05-26 10:23:10 +0600

    Bank Payment Class created

--- src/Question_1/com/paymentUsingBank.java
+++ src/Question_1/com/paymentUsingBank.java
@@ -3,7 +3,7 @@ package Question_1.com;
 class paymentUsingBank implements paymentType{
     @Override
     public boolean pay(double amount) {
-        System.out.println("Payment Through pay pal card");
+        System.out.println("Payment Through Bank");
         return true;
     }
 }
