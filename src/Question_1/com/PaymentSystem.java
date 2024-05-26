3c579a9f891ed3714902dd6636c51fd298623e4d
shaila001 <rahamanshaila2001@gmail.com>
2024-05-26 10:45:24 +0600

    main class created

--- src/Question_1/com/PaymentSystem.java
+++ src/Question_1/com/PaymentSystem.java
@@ -1,4 +1,13 @@
 package Question_1.com;
 
 public class PaymentSystem {
+
+    public static void main(String[] args) {
+
+        PaymentUsingCreditCard pc=new PaymentUsingCreditCard();
+        PaymentUsingPayPal pp=new PaymentUsingPayPal();
+        paymentUsingBank pb=new paymentUsingBank();
+        paymentUsingBkash pbk=new paymentUsingBkash();
+    }
+
 }
